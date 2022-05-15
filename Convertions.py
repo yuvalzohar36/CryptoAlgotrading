2ac5d69ee744f22fad95344ae659bb3c3d8c4f17
יובל בדיחי <badihiyuval2@gmai.com>
2022-05-15 23:11:08 +0300

    Yuval Badihi

--- Convertions.py
+++ Convertions.py
@@ -8,6 +8,7 @@ class Convert:
     def from_to(self, from_currency, to_currency):
         return from_currency in self.json_conversions['TO_' + to_currency]
 
+
     @staticmethod
     def load_json(json_file_name):
         with open(json_file_name) as json_file:
