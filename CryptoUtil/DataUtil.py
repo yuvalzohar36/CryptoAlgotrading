4e464c7c518c1bd23cbcb63465d9103e7a7bc3e9
badihi <badihyuval2@gmail.com>
2022-07-26 19:40:52 +0300

    vesion 0.03

--- CryptoUtil/DataUtil.py
+++ CryptoUtil/DataUtil.py
@@ -1,6 +1,6 @@
 import json
 from datetime import datetime, timedelta
-
+import pandas as pd
 from Indicators.Indicator import Indicator as IndicatorUtil
 
 USERNAME = 'yuvalbadihi'
@@ -24,15 +24,48 @@ class DataUtil:
 
     def request_historical_data(self, symbol, candles_count):
         convert_symbol = symbol + "USDT"
+        temp_date = self.to_date + timedelta(days=1)
         start_time = f"{self.steps * self.mins * candles_count + 60} minutes ago UTC"
         if self.mode == "LIVE":
-            data = self.client.get_historical_klines(symbol + "USDT", str(self.mins) + 'm', start_time)
+            data = self.client.get_historical_klines(convert_symbol, str(self.mins) + 'm', start_time)
         else:
-            from_date = self.to_date - timedelta(minutes=self.steps * self.mins * candles_count) - timedelta(days=1)
-            data = self.client.get_historical_klines(symbol + "USDT", str(self.mins) + 'm',
+            from_date = self.to_date - timedelta(minutes=self.steps * self.mins * candles_count) - timedelta(days=2)
+            data = self.client.get_historical_klines(convert_symbol, str(self.mins) + 'm',
                                                      from_date.strftime("%d %b, %Y"),
-                                                     self.to_date.strftime("%d %b, %Y"))
-        print(data)
+                                                     temp_date.strftime("%d %b, %Y"))
+
+        new_data = []
+        for i in data:
+            new_data.append(i)
+            if str(int(datetime.timestamp(self.to_date))) in str(i[0]):
+                break
+        bars = new_data
+
+
+        for line in bars:  # Keep only first 5 columns, "date" "open" "high" "low" "close"
+            del line[5:]
+        df = pd.DataFrame(bars, columns=['date', 'open', 'high', 'low', 'close'])  # 2 dimensional tabular data
+        new_df = pd.DataFrame(columns=['date', 'open', 'high', 'low', 'close'])  # 2 dimensional tabular data
+
+        high, low, close, open, date = 0, float('inf'), 0, 0, 0
+        counter = 0
+        for index, row in df.iterrows():
+            date = str(row['date'])
+            if counter == 0:
+                open = float(row['open'])
+            if counter % self.steps == 0 and counter != 0:
+                new_df = new_df.append({'date': date, 'low': low, 'high': high, 'open': open, 'close': close},
+                                       ignore_index=True)
+                high, low, close, open, date = 0, float('inf'), 0, 0, 0
+                open = float(row['open'])
+            close = float(row['close'])
+            if float(row['high']) > high:
+                high = float(row['high'])
+            if float(row['low']) < low:
+                low = float(row['low'])
+            counter += 1
+        print(new_df)
+
 
     def set_date(self, date):
         self.to_date = date
@@ -44,5 +77,5 @@ if __name__ == '__main__':
     with open(CONFIGURATION_FILE) as config_file:
         config = json.load(config_file)
     s = DataUtil(config, local_config, "TEST")
-    s.set_date(datetime(2020, 5, 17))
+    s.set_date(datetime(2022, 7, 25, 17, 15))
     s.request_historical_data('BTC', 25)
